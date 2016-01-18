9a9a92fa57f93ba848889d3cda99286cf031259e
ferb <fduy96@gmail.com>
2016-01-18 02:58:22 -0600

    git configs

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+rm ~/.bashrc
+ln -s ~/dotdotdot/.bashrc ~/.bashrc
+
+rm ~/.gitconfig
+ln -s ~/dotdotdot/gitconfig ~/.gitconfig
+
+rm ~/.git-prompt.sh
+ln -s ~/dotdotdot/git-prompt.sh ~/.git-prompt.sh
