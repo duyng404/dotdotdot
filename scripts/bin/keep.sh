25709640b6806694c4eb0332045a5c0215e28cf8
ferb96 <fduy96@gmail.com>
2016-02-24 23:38:50 -0600

    fixed battery

--- scripts/bin/keep.sh
+++ scripts/bin/keep.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-bspc rule -a chromium -o desktop=focused state=floating && /usr/bin/chromium --profile-directory=Default --app-id=hmjkmjkepdijhoojdojkdfohbdgmmhki
+bspc rule -a chromium -o desktop=focused state=pseudo_tiled && /usr/bin/chromium --profile-directory=Default --app-id=hmjkmjkepdijhoojdojkdfohbdgmmhki
