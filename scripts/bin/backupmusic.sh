0a2b0d599b7598e8b1936fdc4d52901c544a0ffe
ferb96 <fduy96@gmail.com>
2017-05-05 01:00:14 +0900

    delete obsolete scripts

--- scripts/bin/backupmusic.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rsync -auP ~/usr/music/ ~/usr/musicarchive/
--- scripts/bin/syncpic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if test -z "$1"
-then
-	# no remote specified
-	rsync -auP --delete ferb@Junketsu:~/usr/music/ ~/usr/music/
-else
-	rsync -auP --delete "$1:~/usr/music/" ~/usr/music/
-fi
