a6eebef808959066401f6ab7a7d501cf455eca21
ferb96 <fduy96@gmail.com>
2018-01-06 00:59:15 -0600

    minor polybar changes

--- scripts/bin/i3lock.sh
+++ scripts/bin/i3lock.sh
@@ -9,5 +9,5 @@ if [[ ! -n `pidof i3lock` ]]; then
 	scrot "$tmpbg"
 	convert "$tmpbg" -brightness-contrast -18x-20 -scale 10% -scale 1000% "$tmpbg"
 	convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-	i3lock -u -i "$tmpbg"
+	i3lock -i "$tmpbg"
 fi
